60cb871303bcc05de0f7ef095141dc4293177fd4
HoaiAn09 <112369207+DuongHoaiAn2k1@users.noreply.github.com>
2024-11-11 03:14:31 +0700

    Update update-user-point.sh

--- docker/cron/update-user-point.sh
+++ docker/cron/update-user-point.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/docker exec ct550-app php artisan user:update-user-points >> /home/hoaian/CT550/backend/docker/cron/logs/update-user-point.log 2>&1
+/usr/bin/docker exec ct550-app php artisan user:update-user-points >> /var/www/html/ct550/docker/cron/logs/update-user-point.log 2>&1
