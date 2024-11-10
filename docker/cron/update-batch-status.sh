d7a40763bdf08a7e352d4eaafe20c42e4e1b54e0
HoaiAn09 <112369207+DuongHoaiAn2k1@users.noreply.github.com>
2024-11-11 03:13:49 +0700

    Update update-batch-status.sh

--- docker/cron/update-batch-status.sh
+++ docker/cron/update-batch-status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/docker exec ct550-app php artisan batch:update-status >> /home/hoaian/CT550/backend/docker/cron/logs/batch-update-status.log 2>&1
+/usr/bin/docker exec ct550-app php artisan batch:update-status >> /var/www/html/ct550/docker/cron/logs/batch-update-status.log 2>&1
