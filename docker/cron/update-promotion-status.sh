53881b8aa22bd7e93c46d194e5ca408eeffb4391
HoaiAn09 <112369207+DuongHoaiAn2k1@users.noreply.github.com>
2024-11-11 03:14:18 +0700

    Update update-promotion-status.sh

--- docker/cron/update-promotion-status.sh
+++ docker/cron/update-promotion-status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/docker exec ct550-app php artisan app:update-status-promotion >> /home/hoaian/CT550/backend/docker/cron/logs/promotion-update-status.log 2>&1
+/usr/bin/docker exec ct550-app php artisan app:update-status-promotion >> /var/www/html/ct550/docker/cron/logs/promotion-update-status.log 2>&1
