e2e546499672a95f68196d224beb2506e21a533f
HoaiAn09 <112369207+DuongHoaiAn2k1@users.noreply.github.com>
2024-11-11 03:14:04 +0700

    Update update-order-status.sh

--- docker/cron/update-order-status.sh
+++ docker/cron/update-order-status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/docker exec ct550-app php artisan app:update-order-status >> /home/hoaian/CT550/backend/docker/cron/logs/order-update-status.log 2>&1
+/usr/bin/docker exec ct550-app php artisan app:update-order-status >> /var/www/html/ct550/docker/cron/logs/order-update-status.log 2>&1
