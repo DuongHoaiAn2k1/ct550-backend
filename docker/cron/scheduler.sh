e037fa6c8bf4f5178bdcfb5b3ebe73a08944e293
HoaiAn09 <112369207+DuongHoaiAn2k1@users.noreply.github.com>
2024-11-11 03:13:30 +0700

    Update scheduler.sh

--- docker/cron/scheduler.sh
+++ docker/cron/scheduler.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/hoaian/CT550/backend
+cd /var/www/html/ct550
 
 # Chạy lệnh php artisan schedule:run trong container
 # /usr/bin/docker exec ct550-app php artisan schedule:run >> /home/hoaian/CT550/backend/docker/cron/logs/cron.log 2>&1
@@ -7,4 +7,4 @@ cd /home/hoaian/CT550/backend
 # Chạy lệnh php artisan batch:update-status trong container
 # /usr/bin/docker exec ct550-app php artisan batch:update-status >> /home/hoaian/CT550/backend/docker/cron/logs/batch-update-status.log 2>&1
 
-echo "Cron ran at $(date)" >> /home/hoaian/CT550/backend/docker/cron/logs/cron-test.log
+echo "Cron ran at $(date)" >> /var/www/html/ct550/docker/cron/logs/cron-test.log
